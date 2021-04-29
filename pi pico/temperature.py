1e1d3eb608403876fcb7783eadcf8e93296b695c
Mr Green <dwgreen@hwdsb.on.ca>
2021-04-29 07:01:34 -0400

    added rounding to print out

--- pi pico/temperature.py
+++ pi pico/temperature.py
@@ -10,5 +10,5 @@ conversion_factor = 3.3 / (65535)
 while True:
     reading = sensor_temp.read_u16() * conversion_factor
     temperature = 27 - (reading - 0.706)/0.001721
-    print(temperature)
+    print(round(temperature, 1))
     utime.sleep(2)
